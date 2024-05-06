20a30f70518c7f1eb291f6fc6e75f635a2eb997d
Fábio Vieira <fabio.gv003@gmail.com>
2024-05-06 09:48:25 -0300

    Little improvement in board printing

--- src/chess/pieces/KIng.java
+++ src/chess/pieces/KIng.java
@@ -5,9 +5,9 @@ import chess.ChessPiece;
 import chess.Color;
 
 
-public class KIng extends ChessPiece{
+public class King extends ChessPiece{
     
-    public KIng(Board board, Color color) {
+    public King(Board board, Color color) {
         super(board, color);
     }
     
